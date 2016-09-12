fd3be1dbd98f1eeb623ec4850b716df1766e2570
Alexander Wallin <office@alexanderwallin.com>
2016-09-12 16:10:14 +0100

    Creates effect chain plugin with public addEffect(node) and removeEffect(node) methods

---
+++ src/index.js
@@ -0,0 +1,122 @@
+(function() {
+
+// ---------- Core extensions ----------- //
+
+
+/**
+ * Sets up the effects chain intermediary.
+ */
+HowlerGlobal.prototype._setup = (function(_super) {
+  return function(o) {
+    var self = this || Howler;
+
+    // List of effects
+    self._effects = [];
+
+    // Input and output nodes
+    self._effectChain = createGain();
+    self._effectChainOut = createGain();
+
+    // Connect nodes to master gain
+    if (self.masterGain) {
+      self._effectChain.connect(self._effectChainOut);
+      self._effectChainOut.connect(self.masterGain);
+    }
+
+    return _super.call(self, o);
+  }
+})(HowlerGlobal.prototype._setup);
+
+/**
+ * Rewires a created sound from being connected directly to
+ * Howler.masterGain to being connect to the effect chain's
+ * input node.
+ */
+Sound.prototype.create = (function(_super) {
+  return function() {
+    var self = _super.call(this);
+
+    if (self._parent._webAudio && Howler._effectChain) {
+      self._node.disconnect(Howler.masterGain);
+      self._node.connect(Howler._effectChain);
+    }
+
+    return self;
+  }
+})(Sound.prototype.create)
+
+
+// ---------- Effect chain methods ----------- //
+
+
+/**
+ * Adds an effect to the effects chain
+ *
+ * @param  {AudioNode} An effect node
+ * @return {Howler}    Self
+ */
+HowlerGlobal.prototype.addEffect = function(node) {
+  var self = this || Howler;
+
+  var lastEffect = self._effects[self._effects.length - 1];
+  if (lastEffect) {
+    lastEffect.disconnect();
+    lastEffect.connect(node);
+  }
+  else {
+    self._effectChain.disconnect();
+    self._effectChain.connect(node);
+  }
+
+  node.connect(self._effectChainOut);
+  self._effects.push(node);
+
+  return self;
+}
+
+/**
+ * Removes an effect from the effects chain.
+ *
+ * @param  {AudioNode} A previously added effect
+ * @return {Howler}    Self
+ */
+HowlerGlobal.prototype.removeEffect = function(node) {
+  var self = this || Howler;
+
+  var effectIndex = self._effects.indexOf(node);
+
+  // If the effect doesn't exist, print a warning and exit early
+  if (effectIndex === -1) {
+    console.warn(
+      '[Howler effects chain] Cannot remove the effect as ' +
+      'it is not added to the chain:', node);
+    return self;
+  }
+
+  var preceedingNode = effectIndex === 0 ? self._effectChain : self._effects[effectIndex - 1];
+  var succeedingNode = effectIndex === self._effects.length - 1 ? self._effectChainOut : self._effects[effectIndex + 1];
+
+  node.disconnect();
+  preceedingNode.disconnect();
+  preceedingNode.connect(succeedingNode);
+
+  self._effects = self._effects.filter(function(effect) {
+    return effect !== node;
+  });
+
+  return self;
+}
+
+
+// ---------- Helpers ----------- //
+
+/**
+ * Creates and returns a gain node.
+ */
+function createGain() {
+  return typeof Howler.ctx.createGain === 'undefined'
+    ? Howler.ctx.createGainNode()
+    : Howler.ctx.createGain();
+}
+
+})()
